c1fdc2b83985600ebd173b0cad77a37cba8222dc
JaeWon1717 <class17@kakao.com>
2023-04-11 19:46:26 +0900

    channel_calBackProject

--- channel_calBackProject.cpp
+++ channel_calBackProject.cpp
@@ -1,6 +1,6 @@
 /////////////////////////////////////////////////////////////////
 //                                                             //
-//                     히스토그램 역투영                       //
+//             2채널 행렬의 히스토그램 역투영                  //
 //                                                             //
 //  역투영한 행렬에서 높은 값을 갖는 요소는 입력행렬에서 해당  //
 //  위치의 화소의 값이 히스토그램의 빈도수가 높은 값임.        //
@@ -30,27 +30,36 @@ using namespace std;
 
 int main()
 {
-	uchar dataA[16] = { 0,  0,   0, 0,
-					1,  1,   3, 5,
-					6,  1,   1, 3,
-						4,  3,   1, 7 };
-	Mat A(4, 4, CV_8U, dataA);
+	uchar dataA[16] = { 0, 0,   0, 0,
+					1, 1,   1, 1,
+					1, 1,   1, 2,
+						1, 2,   1, 3 };
+	Mat A(4, 2, CV_8UC2, dataA);   // 2채널, CV_8UC2 자료형의 4 x 2 행렬 A
 	cout << "A=" << A << endl;
 
-	int    histSize[] = { 4 };
-	float  valueRange[] = { 0, 8 };
-	const  float* ranges[] = { valueRange };
-	int channels[] = { 0 };
-	int dims = 1;
+	int    histSize[] = { 4, 4 };
+	float  range1[] = { 0, 4 };
+	float  range2[] = { 0, 4 };
+	const  float* ranges[] = { range1, range2 };
+	int channels[] = { 0, 1 };
+	int dims = 2;
 
 	Mat hist;
-	calcHist(&A, 1, channels, Mat(), hist, dims, histSize, ranges, true);   // 히스토그램 hist 생성
-	// 히스토그램 계산할 입력영상=A, 영상의 갯수 nimage=1,  channels[0]=0번 채널,
+	calcHist(&A, 1, channels, Mat(), hist, dims, histSize, ranges, true);  // 히스토그램 hist 생성
+	// 히스토그램 계산할 입력영상=srcImage, 영상의 갯수 nimage=1,  channels[] = {0, 1},
 	// mask=Mat():모든화소가 히스토그램계산에 참여, 출력히스토그램 hist, 
-	// dims=1차원, histSize=빈의 갯수 4개, uniform=true:등간격
+	// dims=2차원, histSize=빈의 갯수(4, 4), uniform=디폴트:등간격
+	//  hist는 4X4 행렬
+	//
+	// 위 케이스에서는..
+	//    hist(0,0) = 2 : 0채널과 1채널의 값이 (0,0)인 경우가 모두 2개
+	//    hist(1,1) = 3 : 0채널과 1채널의 값이 (1,1)인 경우가 모두 3개
+	//    hist(1,2) = 2 : 0채널과 1채널의 값이 (1,2)인 경우가 모두 2개
+	//    hist(1,3) = 1 : 0채널과 1채널의 값이 (1,3)인 경우가 모두 1개
+	//    나머지는 값이 없으므로 모두 0 
 	cout << "hist=" << hist << endl;
 
-	Mat backProject;    // 역투영 행렬 
+	Mat backProject;
 	calcBackProject(&A, 1, channels, hist, backProject, ranges);
 	// 행렬 A의 히스토그램 hist를 이용하여 역투영행렬 backProject 계산
 
